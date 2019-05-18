762f34a78c4df1100b6fd03d094afb49e56da6e8
Horno <i.palacin.a@gmail.com>
2019-05-18 14:26:29 +0200

    Levenshtein algorithm finnished

--- corrector.py
+++ corrector.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 import sys
 
+    
+def save_original_text():
+    if len(sys.argv) > 1:
+        file_d = open(sys.argv[2])
+        text = file_d.read()
+        file_d.close() 
+    else:
+        file_d = sys.stdin
+        text = file_d.read()
+    
+    return text.rstrip("\n")
+
+def save_dictionary():
+    if len(sys.argv) > 1:
+        file_d = open(sys.argv[1])
+        text = file_d.read()
+        file_d.close() 
+    else:
+        file_d = sys.stdin
+        text = file_d.read()
+    
+    return text.rstrip("\n").split()
+
 ''' Levenshtein distance algorithm '''
 def lev_distance(word_1, word_2):
     len1 = len(word_1)
@@ -13,39 +36,37 @@ def lev_distance(word_1, word_2):
         return len1
         
     cost = 0 if word_1[-1] == word_2[-1] else 1
-    return min(
-            lev_distance(word_1[:-1], word_2) + 1,
-            lev_distance(word_1, word_2[:-1]) + 1,
-            lev_distance(word_1[:-1], word_2[:-1]) + cost
-            )
     
-def save_original_text():
-    if len(sys.argv) > 1:
-        file_d = open(sys.argv[2])
-        text = file_d.read()
-        file_d.close() 
-    else:
-        file_d = sys.stdin
-        text = file_d.read()
-    
-    return text
-def save_dictionary():
-    if len(sys.argv) > 1:
-        file_d = open(sys.argv[2])
-        text = file_d.read()
-        file_d.close() 
-    else:
-        file_d = sys.stdin
-        text = file_d.read()
-    
-    return text
+    return min(lev_distance(word_1[:-1], word_2) + 1,
+               lev_distance(word_1, word_2[:-1]) + 1,
+               lev_distance(word_1[:-1], word_2[:-1]) + cost)
+
+
+def check_distances(original, dictionary): #TODO Temporal
+    min_dist = -1
+    for word in dictionary:
+        distance = lev_distance(original, word)
+        print(word + " + " + original + " --> " + str(distance))
+
 
 def main():
-    original = save_original_text()
     dictionary = save_dictionary()
-    
+    original = save_original_text() 
+    check_distances(original,dictionary)
+
 
 if __name__=="__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
 
 
