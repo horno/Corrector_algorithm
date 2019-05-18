f09269120fc6ca43241c1f31c22309e980141630
Horno <i.palacin.a@gmail.com>
2019-05-18 20:49:21 +0200

    Script improvement made

--- corrector.py
+++ corrector.py
@@ -41,40 +41,49 @@ def put_editions(editions):
         print(editions)
 
 ''' Levenshtein distance algorithm '''
-def lev_distance(word_1, word_2):
+def lev_distance(word_1, word_2, min_dist, editions = 0):
     len1 = len(word_1)
     len2 = len(word_2)
     cost = 0
    
     if len1 == 0:
-        return len2
+        return len2 + editions
     if len2 == 0:
-        return len1
-        
+        return len1 + editions
+    if editions >= min_dist:
+        return min_dist
+
     cost = 0 if word_1[-1] == word_2[-1] else 1
     
-    return min(lev_distance(word_1[:-1], word_2) + 1,
-               lev_distance(word_1, word_2[:-1]) + 1,
-               lev_distance(word_1[:-1], word_2[:-1]) + cost)
+    case_1 = lev_distance(word_1[:-1], word_2, min_dist, editions + 1)
+    case_2 = lev_distance(word_1, word_2[:-1], min_dist, editions + 1)
+    case_3 = lev_distance(word_1[:-1], word_2[:-1], min_dist, editions + cost)
+    return min(case_1, case_2, case_3)
+
+
 
 
 def check_distances(original, corrected, dictionary, editions): 
     if not original:
         return corrected, editions
-    correct_word, distance = compare_with_dict(original[0], dictionary, "", 99)#TODO Canviar per màxim
+    correct_word, distance = compare_with_dict(original[0], dictionary, "", sys.maxsize)#TODO Canviar per màxim
     return check_distances(original[1:], corrected + " " + correct_word, dictionary, editions + distance)
 
-def compare_with_dict(tocheck_word, dictionary, checked_word, distance):
-    if not dictionary or distance == 0:
-        return checked_word, distance
+def compare_with_dict(tocheck_word, dictionary, checked_word, minimum_distance):
+    if not dictionary or minimum_distance == 0:
+        return checked_word, minimum_distance
+    
     #print("Checking " + tocheck_word + " with " + dictionary[0])
+    
     checking = dictionary[0]
-    dist = lev_distance(tocheck_word,checking)
+    dist = lev_distance(tocheck_word, checking, minimum_distance)
+    
     #print("Distance: " + str(dist))
-    if dist < distance:
+    
+    if dist < minimum_distance:
         return compare_with_dict(tocheck_word, dictionary[1:], checking, dist)
     else:
-        return compare_with_dict(tocheck_word, dictionary[1:], checked_word, distance)
+        return compare_with_dict(tocheck_word, dictionary[1:], checked_word, minimum_distance)
 
 def main():
     dictionary = save_dictionary()
