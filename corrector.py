fe7ac73d7b4426dfaec0e0e9fc4df37e08a8637f
Horno <i.palacin.a@gmail.com>
2019-05-19 22:12:41 +0200

    Finnished

--- corrector.py
+++ corrector.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
-
+''' Levenshtein distance algorithm '''
 import sys
 
 def save_original_text():
+    ''' Reads the text to correct from a file '''
     if len(sys.argv) > 1:
         file_d = open(sys.argv[2], "r")
         text = file_d.read()
-        file_d.close() 
+        file_d.close()
     else:
         file_d = sys.stdin
         text = file_d.read()
-    
+
     return text.rstrip("\n").split()
 
 def save_dictionary():
+    ''' Reads the dictionary from a file '''
     if len(sys.argv) > 1:
         file_d = open(sys.argv[1], "r")
         text = file_d.read()
-        file_d.close() 
+        file_d.close()
     else:
         file_d = sys.stdin
         text = file_d.read()
-    
+
     return text.rstrip("\n").split()
 
 def put_correct_text(text):
+    ''' Saves the correct text in a file '''
     if len(sys.argv) > 1:
         file_d = open(sys.argv[3], "w")
         file_d.write(text)
@@ -33,6 +36,7 @@ def put_correct_text(text):
         print(text)
 
 def put_editions(editions):
+    ''' Saves the correct text in a file '''
     if len(sys.argv) > 1:
         file_d = open(sys.argv[4], "w")
         file_d.write(str(editions))
@@ -41,40 +45,43 @@ def put_editions(editions):
         print(editions)
 
 def check_distances(original, corrected, dictionary, editions):
-    corrected_words=corrected
-    num_editions=editions
+    ''' Iterates recursively through the words to correct '''
+    corrected_words = corrected
+    num_editions = editions
     for tocheck_word in original:
-        minimum_distance=sys.maxsize
+        minimum_distance = sys.maxsize
         for checking in dictionary:
             dist = lev_distance(tocheck_word, checking)
-            if dist<minimum_distance:
-                correct=checking
-                minimum_distance=dist
-        corrected_words=corrected_words+" "+correct
-        num_editions=num_editions+minimum_distance   
-    print(num_editions)
+            if dist < minimum_distance:
+                correct = checking
+                minimum_distance = dist
+        corrected_words = corrected_words+" "+correct
+        num_editions = num_editions+minimum_distance
     return corrected_words, num_editions
 
 def lev_distance(str1, str2):
-    d=dict()
+    ''' Levenshtein distance algorithm '''
+    dic = dict()
     for i in range(len(str1)+1):
-        d[i]=dict()
-        d[i][0]=i
+        dic[i] = dict()
+        dic[i][0] = i
     for i in range(len(str2)+1):
-        d[0][i] = i
+        dic[0][i] = i
     for i in range(1, len(str1)+1):
         for j in range(1, len(str2)+1):
-            d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1, d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
-    return d[len(str1)][len(str2)]
+            dic[i][j] = min(dic[i][j-1]+1, dic[i-1][j]+1,
+                            dic[i-1][j-1]+(not str1[i-1] == str2[j-1]))
+    return dic[len(str1)][len(str2)]
 
 def main():
+    ''' Main function '''
     dictionary = save_dictionary()
-    original = save_original_text() 
+    original = save_original_text()
 
     corrected_text, editions = check_distances(original, "", dictionary, 0)
-    
+
     put_correct_text(corrected_text[1:])
     put_editions(editions)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- corrector_r.py
+++ corrector_r.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
+''' Recursive corrector '''
 import sys
 
-    
+
 def save_original_text():
+    ''' Reads the text to correct from a file '''
     if len(sys.argv) > 1:
         file_d = open(sys.argv[2], "r")
         text = file_d.read()
-        file_d.close() 
+        file_d.close()
     else:
         file_d = sys.stdin
         text = file_d.read()
-    
+
     return text.rstrip("\n").split()
 
 def save_dictionary():
+    ''' Reads the dictionary from a file '''
     if len(sys.argv) > 1:
         file_d = open(sys.argv[1], "r")
         text = file_d.read()
-        file_d.close() 
+        file_d.close()
     else:
         file_d = sys.stdin
         text = file_d.read()
-    
+
     return text.rstrip("\n").split()
 
 def put_correct_text(text):
+    ''' Saves the correct text in a file '''
     if len(sys.argv) > 1:
         file_d = open(sys.argv[3], "w")
         file_d.write(text)
@@ -33,6 +37,8 @@ def put_correct_text(text):
         print(text)
 
 def put_editions(editions):
+    ''' Saves the number of editions in a file '''
+
     if len(sys.argv) > 1:
         file_d = open(sys.argv[4], "w")
         file_d.write(str(editions))
@@ -40,12 +46,13 @@ def put_editions(editions):
     else:
         print(editions)
 
-''' Levenshtein distance algorithm '''
-def lev_distance(word_1, word_2, min_dist, editions = 0):
+def lev_distance(word_1, word_2, min_dist, editions=0):
+    ''' Levenshtein distance algorithm '''
+
     len1 = len(word_1)
     len2 = len(word_2)
     cost = 0
-   
+
     if len1 == 0:
         return len2 + editions
     if len2 == 0:
@@ -54,55 +61,49 @@ def lev_distance(word_1, word_2, min_dist, editions = 0):
         return min_dist
 
     cost = 0 if word_1[-1] == word_2[-1] else 1
-    
+
     case_1 = lev_distance(word_1[:-1], word_2, min_dist, editions + 1)
     case_2 = lev_distance(word_1, word_2[:-1], min_dist, editions + 1)
     case_3 = lev_distance(word_1[:-1], word_2[:-1], min_dist, editions + cost)
     return min(case_1, case_2, case_3)
 
 
-def check_distances(original, corrected, dictionary, editions): 
+def check_distances(original, corrected, dictionary, editions):
+    ''' Iterates recursively through the words to correct '''
+
     if not original:
         return corrected, editions
     correct_word, distance = compare_with_dict(original[0], dictionary, "", sys.maxsize)
-    return check_distances(original[1:], corrected + " " + correct_word, dictionary, editions + distance)
+    return check_distances(original[1:], corrected + " " + correct_word,
+                           dictionary, editions + distance)
 
 def compare_with_dict(tocheck_word, dictionary, checked_word, minimum_distance):
+    ''' Iterates recursively through the words of the dictionary '''
+
     if not dictionary or minimum_distance == 0:
         return checked_word, minimum_distance
-    
-    
+
     checking = dictionary[0]
-    if (len(checking) - len(tocheck_word)) > minimum_distance or len(checking) - len(tocheck_word) < -minimum_distance:
-        return compare_with_dict(tocheck_word, dictionary[1:], checked_word, minimum_distance)
-   
-    dist = lev_distance(tocheck_word, checking, minimum_distance)
-    
-    if dist < minimum_distance:
-        return compare_with_dict(tocheck_word, dictionary[1:], checking, dist)
-    else:
+    if ((len(checking) - len(tocheck_word)) > minimum_distance or
+            len(checking) - len(tocheck_word) < -minimum_distance):
         return compare_with_dict(tocheck_word, dictionary[1:], checked_word, minimum_distance)
 
+    dist = lev_distance(tocheck_word, checking, minimum_distance)
+
+    if dist < minimum_distance:
+        return compare_with_dict(tocheck_word, dictionary[1:], checking, dist)
+
+    return compare_with_dict(tocheck_word, dictionary[1:], checked_word, minimum_distance)
+
 def main():
+    ''' Main function '''
+
     dictionary = save_dictionary()
-    original = save_original_text() 
+    original = save_original_text()
 
     corrected_text, editions = check_distances(original, "", dictionary, 0)
     put_correct_text(corrected_text[1:])
     put_editions(editions)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
